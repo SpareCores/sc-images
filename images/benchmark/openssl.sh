0649f805f109ef38b99eca89f4412da2a5940294
bra-fsn <nagy.attila@gmail.com>
2024-05-23 14:52:44 +0200

    openssl tests

---
+++ images/benchmark/openssl.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CMD="openssl speed -mr -mlock -elapsed -multi $(nproc)"
+
+# https://github.com/openssl/openssl/issues/22545
+echo blake2b512 sha256 sha512 \
+        sha3-256 sha3-512 shake128 shake256 \
+        aes-256-cbc aria-256-cbc \
+        camellia-256-cbc sm4-cbc | \
+xargs -n 1 ${CMD} -evp
+
+${CMD} rsa2048 ECP-384 X25519 X448
