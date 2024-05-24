48b79ad124c365885c0410b40eed8a62d038d143
bra-fsn <nagy.attila@gmail.com>
2024-05-24 12:08:01 +0200

    redirect to stdout

--- images/benchmark/bw_mem.sh
+++ images/benchmark/bw_mem.sh
@@ -5,6 +5,7 @@ for what in rd wr rdwr
     for size in 16k 256k 1M 2M 4M 8M 16M 32M 64M 256M 512M
       do
         echo -n "$what "
-        bw_mem -P $(nproc) $size $what
+        # writes to stderr
+        bw_mem -P $(nproc) $size $what 2>&1
       done
   done
--- images/benchmark/openssl.sh
+++ images/benchmark/openssl.sh
@@ -7,6 +7,6 @@ echo blake2b512 sha256 sha512 \
         sha3-256 sha3-512 shake128 shake256 \
         aes-256-cbc aria-256-cbc \
         camellia-256-cbc sm4-cbc | \
-xargs -n 1 ${CMD} -evp
+xargs -n 1 ${CMD} -evp 2>&1
 
-${CMD} rsa2048 ECP-384 X25519 X448
+${CMD} rsa2048 ECP-384 X25519 X448 2>&1
