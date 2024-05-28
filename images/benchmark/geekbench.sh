efefe2fffbcd89e4233380d2d1835ce3588f39ab
bra-fsn <nagy.attila@gmail.com>
2024-05-28 22:09:01 +0200

    Run preview in non-pro mode

--- images/benchmark/geekbench.sh
+++ images/benchmark/geekbench.sh
@@ -4,5 +4,13 @@
 LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64 openssl aes-256-cbc -d -pass env:BENCHMARK_SECRETS_PASSPHRASE \
   -pbkdf2 -iter 100000 -in /secrets.enc -out - | tar zxpf - -C /
 
-# write the JSON output to stderr, so we can gather it separately from the stdout
-/usr/local/geekbench-$(uname -m)/geekbench6 --export-json /dev/stderr --no-upload
+arch="$(uname -m)"
+
+if [ "${arch}" = aarch64 ]; then
+  # http://support.primatelabs.com/discussions/geekbench/83083-geekbench-6-preview-license
+  # preview doesn't support pro mode
+  /usr/local/geekbench-${arch}/geekbench6
+else
+  # write the JSON output to stderr, so we can gather it separately from the stdout
+  /usr/local/geekbench-${arch}/geekbench6 --export-json /dev/stderr --no-upload
+fi
