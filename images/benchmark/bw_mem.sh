36ca659fb12c62c2431eb746a9cb2c75ac0891c5
bra-fsn <nagy.attila@gmail.com>
2024-05-31 16:52:07 +0200

    only print on success

--- images/benchmark/bw_mem.sh
+++ images/benchmark/bw_mem.sh
@@ -4,8 +4,10 @@ for what in rd wr rdwr
   do
     for size in 16k 256k 1M 2M 4M 8M 16M 32M 64M 256M 512M
       do
-        echo -n "$what "
         # writes to stderr
-        nice -n -20 bw_mem -P $(nproc) $size $what 2>&1
+        result=$(nice -n -20 bw_mem -P $(nproc) $size $what 2>&1)
+        if [ $? -eq 0 ]; then
+          echo $what $result
+        fi
       done
   done
