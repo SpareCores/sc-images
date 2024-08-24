804b0a0c28a2cc46c184efcb8f5e2465fbf3631b
bra-fsn <nagy.attila@gmail.com>
2024-08-24 23:39:34 +0200

    Switch to memtier

--- images/benchmark-redis/benchmark.sh
+++ images/benchmark-redis/benchmark.sh
@@ -2,11 +2,8 @@
 
 base_port=7000
 procs=$(nproc)
-mkdir /results
-
-increment=50000
-runtime_target=5
-n=100000
+threads="1 2 4"
+pipelines="1 16 64 128 256"
 
 for i in $(seq $procs)
 do
@@ -17,58 +14,42 @@ done
 
 sleep 5
 
-while true; do
-    # Start the benchmark and measure its runtime
-    start_time=$(date +%s)
-    redis-benchmark -p $((base_port + 1)) -t SET -q -n $n > /dev/null 2>&1
-    benchmark_status=$?
-    # If redis-benchmark failed, exit the loop
-    if [ "$benchmark_status" -ne 0 ]; then
-        echo "redis-benchmark failed to run with -n $n. Exiting."
-        exit 1
-    fi
-
-    end_time=$(date +%s)
-
-    # Calculate elapsed time
-    elapsed_time=$((end_time - start_time))
-
-    # Check if elapsed time is greater than or equal to preset time
-    if [ "$elapsed_time" -ge "$runtime_target" ]; then
-        echo "Benchmark with -n $n ran for $elapsed_time seconds."
-        break
-    fi
-
-    # Increase the parameter for the next iteration
-    n=$((n + increment))
-done
-
-for test in PING_INLINE PING_MBULK SET GET INCR RPUSH LPOP RPOP SADD \
-  HSET SPOP ZADD ZPOPMIN LRANGE_600 MSET XADD
+for thread in $threads
 do
-  pids=""
-  for i in $(seq $procs)
+  for pipeline in $pipelines
   do
-    port=$((base_port + i))
-    if [ "$test" = "PING_INLINE" ]; then
-      # only write CSV header on the first test
-      redis-benchmark -p $port -t $test --csv -n $n >> /results/$port &
-    else
-      # and remove it from subsequent ones
-      redis-benchmark -p $port -t $test --csv -n $n | grep -v avg_latency_ms >> /results/$port &
-    fi
-    pids="$pids $!"
-  done
+    # dump DBs in each round
+    for i in $(seq $procs)
+    do
+      port=$((base_port + i))
+      redis-cli -p $port flushall > /dev/null 2>&1 &
+    done
+    # wait a little
+    sleep 1
+    pids=""
+    for i in $(seq $procs)
+    do
+      port=$((base_port + i))
+      memtier_benchmark -p $port --ratio=1:0 --test-time=10 -t $thread --hide-histogram --pipeline=$pipeline --json-out-file=/tmp/port=$port,t=$thread,p=$pipeline -o /dev/null &
+      pids="$pids $!"
+    done
 
-  # Wait for only the processes from the second loop to finish
-  for pid in $pids; do
-    wait "$pid"
+    # Wait for only the processes from the second loop to finish
+    for pid in $pids; do
+      wait "$pid"
+    done
   done
 done
 
-for i in $(seq $procs)
+for thread in $threads
 do
-  port=$((base_port + i))
-  echo "Results for redis-benchmark#$port"
-  cat /results/$port
+  for pipeline in $pipelines
+  do
+    for i in $(seq $procs)
+    do
+      port=$((base_port + i))
+      echo "#### threads=$thread,pipeline=$pipeline,port=$port"
+      cat /tmp/port=$port,t=$thread,p=$pipeline
+    done
+  done
 done
