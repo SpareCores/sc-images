6c183f7f4e3b6c09410ec4d3f635bf6709293fdb
bra-fsn <nagy.attila@gmail.com>
2024-09-08 20:57:01 +0200

    Copy-paste remnant

--- images/benchmark-web/benchmark.py
+++ images/benchmark-web/benchmark.py
@@ -176,7 +176,6 @@ def parse_outputs(outputs):
 servers = start_server()
 header = "size,threads,connections,rps,latency,failed,server_usr,server_sys,client_usr,client_sys"
 print(",".join([f'"{word}"' for word in header.split(',')]))
-# we start a redis-memtier_benchmark pair for each CPU, so using more threads doesn't improve performance
 for size in ("1k", "16k", "64k", "256k", "512k"):
     for threadmulti in (1, 2, 4):
         for connsmulti in (1, 2, 4, 8, 16, 32):
