fdbccd2ff4c30322cb7d8d9177268763d41a8aa9
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-01-27 00:07:14 +0100

    DRY command

--- images/llm/benchmark.py
+++ images/llm/benchmark.py
@@ -150,14 +150,12 @@ for model in models:
     model_path = path.join(cli_args.models_dir, model)
     ngl = max_ngl(model_path)
     logger.debug(f"Using ngl {ngl} for model {model}")
+
+    cmd = COMMAND + ["-m", model_path, "-ngl", str(ngl)]
     for n_prompt in N_PROMPTS:
         logger.debug(f"Processing {n_prompt} tokens")
         try:
-            run(
-                COMMAND
-                + ["-m", model_path, "-ngl", str(ngl), "-p", str(n_prompt), "-n", "0"],
-                timeout=TIMEOUT,
-            )
+            run(cmd + ["-p", str(n_prompt), "-n", "0"], timeout=TIMEOUT)
         except Exception as e:
             logger.error(
                 f"Failed to process model {model} while processing {n_prompt} tokens: {e}"
@@ -166,20 +164,7 @@ for model in models:
     for n_generation in N_GENERATIONS:
         logger.debug(f"Generating {n_generation} tokens")
         try:
-            run(
-                COMMAND
-                + [
-                    "-m",
-                    model_path,
-                    "-ngl",
-                    str(ngl),
-                    "-n",
-                    str(n_generation),
-                    "-p",
-                    "0",
-                ],
-                timeout=TIMEOUT,
-            )
+            run(cmd + ["-n", str(n_generation), "-p", "0"], timeout=TIMEOUT)
         except Exception as e:
             logger.error(
                 f"Failed to process model {model} while generating {n_generation} tokens: {e}"
