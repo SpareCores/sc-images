29a2d8302076b6fb3598a1d0df07283af2db1ae3
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-01-26 22:38:24 +0100

    find cuda/cpu llama.cpp path

---
+++ images/llm/benchmark.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from functools import cache
+from subprocess import run
+
+
+@cache
+def get_llama_cpp_path():
+    """Check if GPU/CUDA is available, if not, use CPU-build of llama.cpp."""
+    llama_cpp_path = "/llama_cpp_gpu"
+    result = run(["./llama-cli", "--version"], cwd=llama_cpp_path, capture_output=True)
+    if result.returncode != 0:
+        llama_cpp_path = "/llama_cpp_cpu"
+    return llama_cpp_path
+
+
+print(get_llama_cpp_path())
