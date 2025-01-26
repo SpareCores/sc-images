d183eecba19e37a672c60dcc4619d448dcb242cf
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-01-27 00:12:51 +0100

    DRY loop

--- images/llm/benchmark.py
+++ images/llm/benchmark.py
@@ -68,10 +68,22 @@ COMMAND = [
     "jsonl",
 ]
 
-# prompt processing batch sizes
-N_PROMPTS = [0, 16, 32, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
-# text generation batch sizes
-N_GENERATIONS = [1, 16, 32, 128, 512, 1024, 2048, 4096, 8192]
+BENCHMARKS = [
+    {
+        # prompt processing batch sizes
+        "name": "prompt processing",
+        "iterations": [0, 16, 32, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768],
+        "iteartion_param": "-p",
+        "extra_params": ["-n", "0"],
+    },
+    {
+        # text generation batch sizes
+        "name": "text generation",
+        "iterations": [1, 16, 32, 128, 512, 1024, 2048, 4096, 8192],
+        "iteartion_param": "-n",
+        "extra_params": ["-p", "0"],
+    },
+]
 
 # #############################################################################
 
@@ -152,21 +164,16 @@ for model in models:
     logger.debug(f"Using ngl {ngl} for model {model}")
 
     cmd = COMMAND + ["-m", model_path, "-ngl", str(ngl)]
-    for n_prompt in N_PROMPTS:
-        logger.debug(f"Processing {n_prompt} tokens")
-        try:
-            run(cmd + ["-p", str(n_prompt), "-n", "0"], timeout=TIMEOUT)
-        except Exception as e:
-            logger.error(
-                f"Failed to process model {model} while processing {n_prompt} tokens: {e}"
-            )
-            break
-    for n_generation in N_GENERATIONS:
-        logger.debug(f"Generating {n_generation} tokens")
-        try:
-            run(cmd + ["-n", str(n_generation), "-p", "0"], timeout=TIMEOUT)
-        except Exception as e:
-            logger.error(
-                f"Failed to process model {model} while generating {n_generation} tokens: {e}"
-            )
-            break
+    for benchmark in BENCHMARKS:
+        for iteration in benchmark["iterations"]:
+            logger.debug(f"Benchmarking {benchmark['name']} with {iteration} tokens")
+            try:
+                run(
+                    cmd
+                    + [benchmark["iteartion_param"], str(iteration)]
+                    + benchmark["extra_params"],
+                    timeout=TIMEOUT,
+                )
+            except Exception as e:
+                logger.error(f"Error: {e}")
+                break
