75a2d60b7e68c5b82524228ab5db4caaf5b92c9a
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-02-22 23:09:32 +0100

    fix checking GPU on ARM

--- images/benchmark-llm/benchmark.py
+++ images/benchmark-llm/benchmark.py
@@ -101,6 +101,11 @@ BENCHMARKS = [
 def get_llama_cpp_path():
     """Check if GPU/CUDA is available, if not, use CPU-build of llama.cpp."""
     llama_cpp_path = "/llama_cpp_gpu"
+    # ARM64 doesn't have the CUDA binaries, so we need to use the CPU-build with early return
+    if not path.exists(llama_cpp_path):
+        logger.info("Using CPU-build of llama.cpp")
+        return "/llama_cpp_cpu"
+    # check if we can start the CUDA binary
     result = run(["./llama-cli", "--version"], cwd=llama_cpp_path, capture_output=True)
     if result.returncode != 0:
         llama_cpp_path = "/llama_cpp_cpu"
