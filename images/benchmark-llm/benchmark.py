3ecd06819a7a886fb1f6b46b9ce329c6cc4874c2
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-06-02 23:33:27 +0200

    pin model commit hashes

--- images/benchmark-llm/benchmark.py
+++ images/benchmark-llm/benchmark.py
@@ -34,12 +34,12 @@ cli_parser.add_argument(
     nargs="+",
     type=str,
     default=[
-        "https://huggingface.co/QuantFactory/SmolLM-135M-GGUF/resolve/main/SmolLM-135M.Q4_K_M.gguf",  # 135 M / 100 MB
-        "https://huggingface.co/Qwen/Qwen1.5-0.5B-Chat-GGUF/resolve/main/qwen1_5-0_5b-chat-q4_k_m.gguf",  # 0.5 B / 400 MB
-        "https://huggingface.co/mlabonne/gemma-2b-GGUF/resolve/main/gemma-2b.Q4_K_M.gguf",  # 2B / 1.5 GB
-        "https://huggingface.co/TheBloke/LLaMA-7b-GGUF/resolve/main/llama-7b.Q4_K_M.gguf",  # 7B / 4 GB
-        "https://huggingface.co/microsoft/phi-4-gguf/resolve/main/phi-4-q4.gguf",  # 14 B / 9 GB
-        "https://huggingface.co/unsloth/Llama-3.3-70B-Instruct-GGUF/resolve/main/Llama-3.3-70B-Instruct-Q4_K_M.gguf",  # 70 B / 42 GB
+        "https://huggingface.co/QuantFactory/SmolLM-135M-GGUF/resolve/b35f229108e4c5a4deeccccf9bf75cd46006fa7b/SmolLM-135M.Q4_K_M.gguf",  # 135 M / 100 MB
+        "https://huggingface.co/Qwen/Qwen1.5-0.5B-Chat-GGUF/resolve/cfab082d2fef4a8736ef384dc764c2fb6887f387/qwen1_5-0_5b-chat-q4_k_m.gguf",  # 0.5 B / 400 MB
+        "https://huggingface.co/mlabonne/gemma-2b-GGUF/resolve/8dd617a80664da2853999b6b1206595f1c33daa2/gemma-2b.Q4_K_M.gguf",  # 2B / 1.5 GB
+        "https://huggingface.co/TheBloke/LLaMA-7b-GGUF/resolve/0a1200190c8c1baafaa0b496c599ba68ad3d4056/llama-7b.Q4_K_M.gguf",  # 7B / 4 GB
+        "https://huggingface.co/microsoft/phi-4-gguf/resolve/b1e764cfdbdd0a3ed824d6a8424129eb0a2232ff/phi-4-q4.gguf",  # 14 B / 9 GB
+        "https://huggingface.co/unsloth/Llama-3.3-70B-Instruct-GGUF/resolve/7bcd751e48e909680bd2168359dcf7fb53b12da8/Llama-3.3-70B-Instruct-Q4_K_M.gguf",  # 70 B / 42 GB
     ],
     help="List of URLs of quantized LLM models (gguf) to download and benchmark.",
 )
